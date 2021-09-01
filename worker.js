d2503018013fa2637d62b61d791de1b8487dcf21
Lars <63020111+larrrssss@users.noreply.github.com>
2021-09-01 18:37:33 +0200

    add cache control

--- worker.js
+++ worker.js
@@ -2,13 +2,14 @@ const URL = 'https://raw.githubusercontent.com/larrrssss/icao-to-airport/master/
 
 addEventListener('fetch', (event) => {
   event.respondWith(
-    handleRequest(event.request).catch(
+    handleRequest(event).catch(
       (err) => new Response(err.stack, { status: 500 })
     )
   );
 });
 
-async function handleRequest(request) {
+async function handleRequest(event) {
+  const { request } = event;
   const { pathname } = new URL(request.url);
 
   if (pathname.startsWith('/api')) {
@@ -27,16 +28,28 @@ async function handleRequest(request) {
 
   icao = icao.toUpperCase();
 
-  const response = await fetch(URL);
-  const airports = await response.json();
+  const cacheKey = new Request(new URL(request.url).toString(), request);
+  const cache = caches.default;
 
-  if (airports[icao]) {
-    return new Response(JSON.stringify(airports[icao]), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+  let response = await cache.match(cacheKey);
+
+  if (!response) {
+    const res = await fetch(URL);
+    const airports = await res.json();
+
+    if (airports[icao]) {
+      response = new Response(JSON.stringify(airports[icao]), {
+        headers: { 'Content-Type': 'application/json' },
+      });
+    } else {
+      response = new Response(JSON.stringify({ message: 'Not found', status: 404 }), {
+        headers: { 'Content-Type': 'application/json' },
+      });;
+    }
+    response.headers.append('Cache-Control', 'max-age=2592000');
+
+    event.waitUntil(cache.put(cacheKey, response.clone()));
   }
 
-  return new Response(JSON.stringify({ message: 'Not found', status: 404 }), {
-    headers: { 'Content-Type': 'application/json' },
-  });;
+  return response;
 }
